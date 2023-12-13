262c73caaad234ad9925f8410c0aa917b52a9080
omarstar <obarakat12@gmail.com>
2023-12-13 13:32:30 +0400

    push changes

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,15 +1,27 @@
 //OWL Carousel initializer
 $(document).ready(function() {
-    const options = {
+    const industryOptions = {
         autoPlay: true,
         slideSpeed: 200,
-        paginationSpeed: 500,
+        paginationSpeed: 400,
         items: 5,
         itemsDesktop: [1199, 4],
         itemsDesktopSmall: [979, 3],
         itemsMobile: [479,2],
     }
     
-    $(".owl-carousel").owlCarousel(options);
+    $(".owl-carousel").owlCarousel(industryOptions);
        
+    const heroOptions = {
+        autoPlay: true,
+        slideSpeed: 400,
+        paginationSpeed: 800,
+        items: 1,
+        itemsDesktop: [1199, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsMobile: [479,1],
+    }
+
+    $(".owl-carousel12").owlCarousel(heroOptions);
+
 });
--- sendmail.php
+++ sendmail.php
@@ -1,25 +1,54 @@
 <?php
 
+// if($_SERVER["REQUEST_METHOD"]  == "POST") {
+	$name = cleanInput($_POST['name']);
+    $email = cleanInput($_POST['email']);
+    $subject = cleanInput($_POST['subject']);
+    $phone = cleanInput($_POST['phone']);
+    $message = cleanInput($_POST['message']);
+
 	$message=$_REQUEST['message'];
-	$subject =$_REQUEST['subject'];
+		$subject =$_REQUEST['subject'];
 
-	$name=$_REQUEST['name'];
+		$name=$_REQUEST['name'];
 
-	$email=$_REQUEST['email'];
+		$email=$_REQUEST['email'];
 
-	$formcontent="This Mail come from Infracommunication website \n\nName: $name \n\nSubject: $subject \n\nEmail: $email \n\n$message";
+		$formcontent="This Mail come from Infracommunication website \n\nName: $name \n\nSubject: $subject \n\nEmail: $email \n\n$message";
 
-	$TO1 ="omar@infracom.me"; 	// your mail here
-	$TO2 ="obaraket23@gmail.com"; 	// your mail here
-	//  Sales@Infracommunication.com
+		$TO1 ="omar@infracom.me"; 	// your mail here
+		$TO2 ="Sales@Infracommunication.com"; 	// your mail here
 
-	$send_email=mail($TO1,$subject,$formcontent,"From: ".$name."<".$email.">");
-	$send_email=mail($TO2,$subject,$formcontent,"From: ".$name."<".$email.">");
+		$send_email=mail($TO1,$subject,$formcontent,"From: $name <$email>");
+		$send_email=mail($TO2,$subject,$formcontent,"From: $name <$email>");
 
-	echo "<script>
-	             alert('Message Sent Succesfully'); 
-	             window.location.href='http://infracommunication.com/v2/';  
+		if ($send_email) {
+			echo "<script>
+					alert('Message Sent Successfully');
+					window.location.href='http://infracommunication.com/v2/';
+				</script>";
+		} else {
+			echo "<script>
+					alert('Failed to send message. Please try again.');
+				</script>";
+		}
+		
+	// if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	// 	ECHO "INVALID EMAIL FORMAT, PLEASE TRY AGAIN"
+    //     // Handle invalid email format error
+    //     // Redirect or show an error message
+    // } else {
+        
+    // }
 
-	     </script>";
+	
+
+	function cleanInput($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+
+		return $data;
+	}
 
 ?>
